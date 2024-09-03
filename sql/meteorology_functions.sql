8dcb8d3a76392af5bf7c0812a81a89d656dc84b0
jbianchi81 <jbianchi@ina.gob.ar>
2024-09-03 11:43:42 -0300

    get_series_table function

--- sql/meteorology_functions.sql
+++ sql/meteorology_functions.sql
@@ -373,3 +373,18 @@ end;
 $function$
 
 ;
+
+CREATE FUNCTION get_series_table(tipo varchar) RETURNS varchar
+AS $$
+BEGIN
+    RETURN CASE 
+        WHEN tipo = 'areal'
+            THEN 'series_areal'
+        WHEN tipo = 'raster'
+            THEN 'series_raster'
+        ELSE
+            'series'
+        END;
+END
+$$
+LANGUAGE plpgsql;
